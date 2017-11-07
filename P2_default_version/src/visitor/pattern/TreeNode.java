221a298eaabb8df46bcc5e395030aeddc6f3fd64
alicjaSkurczynska <skurczynska.alicja@gmail.com>
2017-11-07 20:56:46 +0100

    Remove unnecessery files from repository

--- P2_default_version/src/visitor/pattern/TreeNode.java
+++
@@ -1,8 +0,0 @@
-//abstract superclass of both tree elements (simple, binary)
-
-package visitor.pattern;
-
-public interface TreeNode {
-  public abstract String getLabel() ;
-  public abstract void accept(Visitor v);// enable visitation - makes pattern work
-}
